9113b4b71f9560e7a95a4cc4f572a0938a8c43c0
David Fazekas <fazekasda@gmail.com>
2017-01-31 23:17:32 +0100

    setup auto deploy docs

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -12,21 +12,14 @@ eval `ssh-agent -s`
 ssh-add $RSA_key_file
 
 # config git
-echo "Before:"
-git remote -v
-
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config push.default tracking
 GITHUB_REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
 GITHUB_USER=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
 GITHUB_REPO=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
 GITHUB_REPO_SSH_URL="git@github.com:$GITHUB_USER/$GITHUB_REPO.git"
 git remote set-url origin $GITHUB_REPO_SSH_URL
-
-echo "After:"
-git remote -v
-
-git pull
 
 # deploy docs
 mkdocs gh-deploy
